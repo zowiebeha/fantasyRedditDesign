03432fe6c1160cd0c284eeb37ce0f7a2a62422a5
Zowie Beha <113861530+1zzowiebeha@users.noreply.github.com>
2025-09-18 15:10:16 -0400

    Begin refactor & sidebar close animation

--- public/js/btn--toggle-sidebar.js
+++ public/js/btn--toggle-sidebar.js
@@ -1,17 +1,29 @@
 const sidebarToggleButton = document.getElementById('btn--toggle-sidebar');
+const sidebar = document.getElementById('sidebar');
+
+const animating = false;
 
 sidebarToggleButton.addEventListener('click', () => {
-    const isSidebarToggled = sidebarToggleButton.getAttribute('aria-expanded');
-
-    // User tampered with the aria-expanded value.
-    if (isSidebarToggled != 'true' && isSidebarToggled != 'false') {
-        throw new Error(`Invalid aria-expanded value provided: '${isSidebarToggled}'.`)
+    if (!animating) {
+        const isSidebarToggled = sidebarToggleButton.getAttribute('aria-expanded');
+    
+        // User tampered with the aria-expanded value.
+        if (isSidebarToggled != 'true' && isSidebarToggled != 'false') {
+            throw new Error(`Invalid aria-expanded value provided: '${isSidebarToggled}'.`)
+        }
+    
+        const ariaExpandedBoolNegated = isSidebarToggled == 'false' ? 'true' : 'false';
+        
+        // Trigger close animation to occur
+    
+        sidebarToggleButton.setAttribute(
+            'aria-expanded',
+            ariaExpandedBoolNegated
+        );
+        
+        // Wait for current animation to finish
+        sidebarToggleButton.addEventListener('animationend', () => {
+            animating = true;
+        });
     }
-
-    const ariaExpandedBoolNegated = isSidebarToggled == 'false' ? 'true' : 'false';
-
-    sidebarToggleButton.setAttribute(
-        'aria-expanded',
-        ariaExpandedBoolNegated
-    );
 });
